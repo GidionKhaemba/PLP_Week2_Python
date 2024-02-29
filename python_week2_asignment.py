fc31fac33d6493585278a5fbdb19c3679bc578be
Gidion Khaemba <136009761+GidionKhaemba@users.noreply.github.com>
2024-03-01 00:22:46 +0300

    Add files via upload

---
+++ python_week2_asignment.py
@@ -0,0 +1,25 @@
+#Create an empty list called my_list.
+my_list=[]
+
+#Append the following elements to my_list: 10, 20, 30, 40.
+my_list.extend([10, 20, 30, 40])
+
+#Insert the value 15 at the second position in the list.
+print(my_list)
+my_list.insert(1, 15)
+
+#Extend my_list with another list: [50, 60, 70].
+my_list.extend([50, 60, 70])
+print(my_list)
+
+#Remove the last element from my_list.
+my_list.pop()
+print(my_list)
+
+#Sort my_list in ascending order.
+my_list.sort()
+
+#Find and print the index of the value 30 in my_list.
+print(my_list.index(30))
+
+
